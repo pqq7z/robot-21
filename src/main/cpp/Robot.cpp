d6e08532c3b06a4f4f9ffa9263e5a9bf4981ddda
frc <frc@nomail.com>
2021-02-25 22:56:54 -0500

    added auton and shooter support

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -76,6 +76,48 @@ move_step_t mv_3[] =
   {0.0, 0.0, 0.5, 0.0, 0.0},
 };
 
+move_step_t mv_4[] =
+{
+  // delay
+  {0.0, 0.0, 0.0, 0.0, 0.0},
+  // drive
+  {-0.4, 0.0, 2.2, 0.0, 0.0},
+  {0.0, 0.0, 1.0, 0.0, 0.0},
+  {0.0, 0.2, 2.0, 0.0, 0.0},
+  // stop
+  {0.0, 0.0, 0.5, 0.0, 0.0},
+};
+move_step_t mv_5[] =
+{
+  // delay
+  {0.0, 0.0, 0.0, 0.0, 0.0},
+  // drive
+  {0.4, 0.0, 1.5, 0.0, 0.0},
+
+};
+move_step_t mv_6[] = 
+{
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, //forward 5 feet
+  {0.0, -0.2, 2.0, 0.0,0.0},  //90 degrees left
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, // forward 5 feet
+  {0.0, 0.2, 2.0, 0.0, 0.0},  //90 degrees right
+  {-0.4, 0.0, 3.2, 0.0, 0.0}, //15 feet forward
+  {0.0, 0.2, 2.0, 0.0, 0.0},  //90 degrees right
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, //5 feet forward
+  {-0.4, -0.2, 2.0, 0.0, 0.0}, //90 degrees left
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, //5 feet forward
+  {0.0, -0.2, 2.0, 0.0, 0.0}, //90 degrees left
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, // 5 feet forward
+  {-0.0, 0.2, 2.0, 0.0, 0.0}, //90 degrees left
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, // 5 feet forward
+  {0.0, 0.2, 2.0, 0.0, 0.0},  // 90 degrees right
+  {-0.4, 0.0, 3.2, 0.0, 0.0}, //15 feet forwards
+  {0.0, 0.2, 2.0, 0.0, 0.0},  // 90 degrees right
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, // 5 feet forward
+  {0.0, -0.2, 2.0, 0.0, 0.0}, // 90 degrees left
+  {-0.4, 0.0, 1.0, 0.0, 0.0}, // 5 feet forward
+};
+
 move_step_t none[] =
 {
 	{0.0, 0.0, 0.0, 0.0, 0.0},
@@ -83,8 +125,10 @@ move_step_t none[] =
 
 move_seq_t mv = {none, 0};
 
-#if 0
-static void clamp(double &value, const double ul, const double ll)
+double accel_max = 0.02;
+#if 1
+// static 
+void clamp(double &value, const double ul, const double ll)
 {
     if (value < ll)
 	{
@@ -97,7 +141,8 @@ static void clamp(double &value, const double ul, const double ll)
 }
 #endif
 #if 1
-static void scale(double &value, const double deadband, const double ll, const double ul)
+static 
+void scale(double &value, const double deadband, const double ll, const double ul)
 {
 //printf("1: value=%5.2f\n", value);
 
@@ -125,7 +170,7 @@ static void scale(double &value, const double deadband, const double ll, const d
 class Robot : public frc::TimedRobot {
  public:
   Robot() {
-    printf("robot-21 v1.0 %s %s\n", __DATE__, __TIME__);
+    printf("robot-21 v1.1.0 %s %s\n", __DATE__, __TIME__);
 
     m_robotDrive.SetExpiration(0.1);
     m_timer.Start();
@@ -137,6 +182,10 @@ class Robot : public frc::TimedRobot {
 
     frc::SmartDashboard::PutNumber("delay", 3);
     frc::SmartDashboard::PutNumber("auto", 1);
+    frc::SmartDashboard::PutNumber("t", 0);
+    frc::SmartDashboard::PutNumber("y", 0);
+    frc::SmartDashboard::PutNumber("z", 0);
+    frc::SmartDashboard::PutNumber("Shooter",0);
   }
 
   void AutonomousInit() override {
@@ -145,6 +194,11 @@ class Robot : public frc::TimedRobot {
 
     double delay = frc::SmartDashboard::GetNumber("delay", 5);
     double move = frc::SmartDashboard::GetNumber("auto", 1);
+    double t = frc::SmartDashboard::GetNumber("t", 0);
+    double y = frc::SmartDashboard::GetNumber("y", 0);
+    double z = frc::SmartDashboard::GetNumber("z", 0);
+    y=y/10;
+    z=z/10;
 
     mv.steps = mv_default;
     mv.total_steps = SIZEOF_ARRAY(mv_default);
@@ -158,6 +212,24 @@ class Robot : public frc::TimedRobot {
     {
       mv.steps = mv_3;
       mv.total_steps = SIZEOF_ARRAY(mv_3);
+    }
+    else if (move == 4)
+    {
+      mv.steps = mv_4;
+      mv.total_steps = SIZEOF_ARRAY(mv_4);
+    }
+    else if (move == 5)
+    {
+      mv.steps = mv_5;
+      mv.total_steps = SIZEOF_ARRAY(mv_5);
+      mv.steps[1].y = y; 
+      mv.steps[1].t = t; 
+      mv.steps[1].z = z; 
+    }
+    else if (move == 6)
+    {
+      mv.steps = mv_6;
+      mv.total_steps = SIZEOF_ARRAY(mv_6);
     }
 
     t_step_end = m_timer.Get() + delay;
@@ -221,8 +293,8 @@ class Robot : public frc::TimedRobot {
     double dy = y - last_y;
     double dz = z - last_z;
 
-    // clamp(dy, accel_max, -accel_max);
-    // clamp(dz, accel_max, -accel_max);
+    clamp(dy, accel_max, -accel_max);
+    clamp(dz, accel_max, -accel_max);
 
     last_y = last_y + dy;
     last_z = last_z + dz;
@@ -241,6 +313,10 @@ class Robot : public frc::TimedRobot {
 
   void TeleopInit() override 
   {
+     m_shooter = frc::SmartDashboard::GetNumber("Shooter", 5);
+     m_shooter = m_shooter / 10;
+
+     printf("shooter=%5.2f\n", m_shooter);
   }
 
   void TeleopPeriodic() override 
@@ -256,9 +332,9 @@ class Robot : public frc::TimedRobot {
 
     // operator controls
     if (m_sticko.GetRawButton(4))
-      m_telescope.Set(0.6);
+      m_telescope.Set(m_shooter);
     else if (m_sticko.GetRawButton(1))
-      m_telescope.Set(-0.6);
+      m_telescope.Set(-m_shooter);
     else 
       m_telescope.Set(0);
 
@@ -309,6 +385,8 @@ class Robot : public frc::TimedRobot {
   
   double last_z {0};
   double last_y {0};
+
+  double m_shooter {0};
 };
 
 #ifndef RUNNING_FRC_TESTS
